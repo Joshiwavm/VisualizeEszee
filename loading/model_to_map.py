29f7114bcff25113dca941a967ba8260c0bb99c5
Joshiwavm <joshiwa01@gmail.com>
2025-09-05 16:56:28 +0200

    basic implementation done v2

--- loading/model_to_map.py
+++ loading/model_to_map.py
@@ -134,7 +134,7 @@ class MapMaking:
         weights = np.exp(logwt - norm - logz[-1])
 
         # Mask tiny weights
-        m = weights > 1e-4
+        m = weights > 1e-5
         samples = raw_samples[m]
         weights = weights[m]
         weights /= weights.sum()
